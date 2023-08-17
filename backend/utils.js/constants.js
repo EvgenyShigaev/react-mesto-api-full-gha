4bb0b646c656c7b0b58cca7b0ae4cfbf10869528
Evgeny Shigaev <shigaev-evgeniy@yandex.ru>
2023-08-18 00:29:59 +0400

    ПР 15.2

--- backend/utils.js/constants.js
+++ backend/utils.js/constants.js
@@ -1,4 +1,7 @@
-const URL_REGEX = /(https?:\/\/)?([\w.-]+\.[\w]{2,})$/;
+// eslint-disable-next-line no-useless-escape
+const URL_REGEX = /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+
+// /(https?:\/\/)?([\w.-]+\.[\w]{2,})$/;
 
 // const URL_REGEX = /(https?:\/\/)(www)?([a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=])*#?$/;
 
